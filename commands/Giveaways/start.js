10220a6a31b44016f600676290f6f05f7351ff5c
ofigennaya2018 <91879273+ofigennaya2018@users.noreply.github.com>
2021-10-04 16:46:30 +0300

    Update start.js

--- commands/Giveaways/start.js
+++ commands/Giveaways/start.js
@@ -3,7 +3,7 @@ const config = require("../../config.json")
 
 module.exports = {
     config: {
-        name: "start",
+        name: "старт",
         description: "Начинает розыгрыш",
         usage: "[channel] [duration] [winners] [prize]",
         category: "Giveaways",
@@ -13,32 +13,33 @@ module.exports = {
     run: async (client, message, args) => {
         if (config["Giveaway_Options"].giveawayManagerID) {
             if (!message.member.hasPermission('MANAGE_MESSAGES') && !message.member.roles.cache.some((r) => r.id === config["Giveaway_Options"].giveawayManagerID)) {
-                return message.channel.send(':boom: You need to have the \`MANAGE_MESSAGES\` permissions to start giveaways.');
+                return message.channel.send(':boom: Вам нужно иметь \`MANAGE_MESSAGES\` разрешения на раздачу подарков.');
             }
         } else {
             if (!message.member.hasPermission('MANAGE_MESSAGES') && !message.member.roles.cache.some((r) => r.name === "Giveaways")) {
-                return message.channel.send(':boom: You need to have the \`MANAGE_MESSAGES\` permissions to start giveaways.');
+                return message.channel.send(':boom: Вам нужно иметь \`MANAGE_MESSAGES\` разрешения на раздачу подарков.
+');
             }
         }
 
         let giveawayChannel = message.mentions.channels.first();
         if (!giveawayChannel) {
-            return message.channel.send(':boom: Uh oh, I couldn\'t find that channel! Try again!');
+            return message.channel.send(':boom: Ой, я не смог найти этот канал! Попробуйте снова!');
         }
 
         let giveawayDuration = args[1];
         if (!giveawayDuration || isNaN(ms(giveawayDuration))) {
-            return message.channel.send(':boom: Hm. you haven\'t provided a duration. Can you try again?');
+            return message.channel.send(':boom: Хм. вы не указали продолжительность. Вы можете попробовать еще раз?');
         }
 
         let giveawayNumberWinners = args[2];
         if (isNaN(giveawayNumberWinners) || (parseInt(giveawayNumberWinners) <= 0)) {
-            return message.channel.send(':boom: Uh... you haven\'t provided the amount of winners.');
+            return message.channel.send(':boom: Эээ ... вы не указали количество победителей.');
         }
 
         let giveawayPrize = args.slice(3).join(' ');
         if (!giveawayPrize) {
-            return message.channel.send(':boom: Oh, it seems like you didn\'t give me a valid prize!');
+            return message.channel.send(':boom: О, похоже, ты не дал мне действительного приза!');
         }
         if (!config["Giveaway_Options"].showMention && config["Giveaway_Options"].giveawayRoleID && config["Giveaway_Options"].giveawayMention) {
 
@@ -57,8 +58,8 @@ module.exports = {
                     embedFooter: "Подарки",
                     noWinner: "Недостаточно участников для определения победителя!",
                     hostedBy: "Создан: {user}",
-                    winners: "Победитель(и)",
-                    endedAt: "Закончился в",
+                    winners: "Победитель(ля)",
+                    endedAt: "Закончился",
                     units: {
                         seconds: "секунд",
                         minutes: "минут",
@@ -86,7 +87,7 @@ module.exports = {
                     noWinner: "Недостаточно участников для определения победителя!",
                     hostedBy: "Создан: {user}",
                     winners: "Победитель(и)",
-                    endedAt: "Закончился в",
+                    endedAt: "Закончился",
                     units: {
                         seconds: "секунд",
                         minutes: "минут",
@@ -114,7 +115,7 @@ module.exports = {
                     noWinner: "Недостаточно участников для определения победителя!",
                     hostedBy: "Создан: {user}",
                     winners: "Победитель(и)",
-                    endedAt: "Закончился в",
+                    endedAt: "Закончился",
                     units: {
                         seconds: "секунд",
                         minutes: "минут",
@@ -141,7 +142,7 @@ module.exports = {
                     noWinner: "Недостаточно участников для определения победителя!",
                     hostedBy: "Создан: {user}",
                     winners: "Победитель(и)",
-                    endedAt: "Закончился в",
+                    endedAt: "Закончился",
                     units: {
                         seconds: "секунд",
                         minutes: "минут",
@@ -167,7 +168,7 @@ module.exports = {
                     noWinner: "Недостаточно участников для определения победителя!",
                     hostedBy: "Создан: {user}",
                     winners: "Победитель(и)",
-                    endedAt: "Закончился в",
+                    endedAt: "Закончился",
                     units: {
                         seconds: "секунд",
                         minutes: "минут",
