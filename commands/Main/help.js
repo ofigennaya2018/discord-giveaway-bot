0fe42750087ffb8e38645a0fcf34644131c0aeb3
ofigennaya2018 <91879273+ofigennaya2018@users.noreply.github.com>
2021-10-04 18:15:34 +0300

    Update help.js

--- commands/Main/help.js
+++ commands/Main/help.js
@@ -3,11 +3,11 @@ const config = require("../../config.json")
 module.exports = {
   config: {
     name: "help",
-    description: "Get a list of bot commands.",
+    description: "Получить список команд бота.",
     usage: "help",
     category: "Main",
     accessableby: "Everyone",
-    aliases: [], // To add custom aliases just type ["alias1", "alias2"].
+    aliases: [], // Чтобы добавить собственные псевдонимы, просто введите ["alias1", "alias2"].
   },
   run: async (client, message, args) => {
     let avatarOptions = {
@@ -20,45 +20,45 @@ module.exports = {
       .setAuthor(
         client.user.username,
         client.user.displayAvatarURL({ ...avatarOptions }),
-        'https://github.com/ghaku/discord-giveaway-bot'
+        'https://github.com/ofigennaya2018/discord-giveaway-bot'
       )
       .setThumbnail(client.user.displayAvatarURL({ ...avatarOptions }))
       .setTitle('Help')
-      .setURL('https://github.com/ghaku/discord-giveaway-bot')
+      .setURL('https://github.com/ofigennaya2018/discord-giveaway-bot')
       .setColor('7289da')
-      .setDescription(`You want to create a giveaway bot yourself?\n[Check out our Github page!](https://github.com/ghaku/discord-giveaway-bot)`)
+      .setDescription(`Хотите сами создать бесплатного бота?\n[Посетите мою страницу в Github!](https://github.com/ofigennaya2018/discord-giveaway-bot)`)
       .addFields({
         name: `🎉 ${config["Bot_Info"].prefix}start [channel] [duration] [winners] [prize]`,
         value: [
-          'The channel has to be visible to the giveaway bot.',
-          'Duration is stated in a number and a time variable.',
-          'Winners is stated in a positive number.',
-          'Prize can be anything except blank.'
+          'Канал должен быть виден раздачному боту.',
+          'Продолжительность указывается числом и временной переменной.',
+          'Победители указываются положительным числом.',
+          'Приз может быть любым, кроме пустого.'
         ].join('\n')
       }, {
         name: '👥 Example:',
         value: [
           `⌨️ ${config["Bot_Info"].prefix}start #general 10m 1 $9.99 Nitro`,
-          `➡️ Creates a \`10 minute\` long giveaway with \`1\` winner and`,
-          `\`$9.99 Nitro\` as a prize in \`#general\`.`
+          `➡️ Создает на \`10 minute\` конкурс на \`1\` победителя с призом`,
+          `\`МОНЕТЫ\` на канале \`#general\`.`
         ].join('\n')
       }, {
         name: `❌ ${config["Bot_Info"].prefix}end [message-id]`,
         value: 'Message-ID has to be the **ID** of the giveaway message.\n**Not the link!**'
       }, {
         name: '👥 Example:',
-        value: `⌨️ ${config["Bot_Info"].prefix}end 892678258946659587\n➡️ Ends the giveaway with the message-ID \`892678258946659587\`.`
+        value: `⌨️ ${config["Bot_Info"].prefix}end 892678258946659587\n➡️ Завершает розыгрыш сообщением-ID \`892678258946659587\`.`
       }, {
         name: `🔍 ${config["Bot_Info"].prefix}reroll [message-id]`,
         value: 'Message-ID has to be the **ID** of the giveaway message.\n**Not the link!**'
       }, {
         name: '👥 Example:',
-        value: `⌨️ ${config["Bot_Info"].prefix}reroll 892678258946659587\n➡️ Selects new winners for the giveaway with the message-ID \`892678258946659587\`.`
+        value: `⌨️ ${config["Bot_Info"].prefix}reroll 892678258946659587\n➡️ Выбирает новых победителей розыгрыша с сообщением-ID \`892678258946659587\`.`
       })
-      .setFooter('Made with 💖 and discord.js by ghaku', client.user.displayAvatarURL({ ...avatarOptions }))
+      .setFooter('Перевод by SELESTA', client.user.displayAvatarURL({ ...avatarOptions }))
 
     if (message.guild) {
-      message.channel.send('Check your DMs!');
+      message.channel.send('Проверьте свои личные сообщения!');
       message.delete();
       message.author.send(embed);
     } else {
